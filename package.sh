e0f3369d2dc83f58d2fbc8c851ddeed66a4eb4ba
Adam Spofford <adam.spofford@dfinity.org>
2025-12-03 06:00:25 -0800

    Initial commit

---
+++ package.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")"
+die() {
+    echo "$1" >&2
+    exit 1
+}
+command -v jq >/dev/null 2>&1 || die "please install jq"
+
+case $(uname -s) in
+    Linux*)     os="linux";;
+    Darwin*)    os="darwin";;
+    *)          echo "Unsupported OS $(uname -s)"; exit 1;;
+esac
+case $(uname -m) in
+    x86_64*)    arch="x86_64";;
+    arm64*)     arch="arm64";;
+    aarch64*)   arch="arm64";;
+    *)          echo "Unsupported architecture $(uname -m)"; exit 1;;
+esac
+
+v=$(cargo metadata --format-version=1 --no-deps | jq -r '.packages[] | select(.name=="pocket-ic-launcher") | .version')
+source=$(cargo metadata --format-version=1 --no-deps | jq -r '.packages[] | select(.name=="pocket-ic-launcher") | .dependencies[] | select(.name=="pocket-ic") | .source')
+if [[ "$v" = *"+"* ]]; then
+    [[ "$source" = "git+"* ]] || die "package.version is patch but pocket-ic dependency is not git"
+    revstr=${source#"git+https://github.com/dfinity/ic?"}
+    [[ "$revstr" =~ 'rev='[0-9a-f]{40} ]] || die "use the full hash in the pocket-ic version"
+    sha=${revstr#"rev="}
+else
+    [[ "$source" != "git+"* ]] || die "package.version is not patch but pocket-ic dependency is git"
+fi
+name="pocket-ic-launcher-${arch}-${os}-${v}"
+outdir="${1-"dist/${name}"}"
+
+cargo build --release
+mkdir -p "${outdir}"
+cp "target/release/pocket-ic-launcher" "${outdir}/"
+if [[ -z "$sha" ]]; then
+    curl --proto '=https' -sSfL --tlsv1.2 "https://github.com/dfinity/pocketic/releases/download/${v}/pocket-ic-${arch}-${os}.gz" -o "${outdir}/pocket-ic.gz"
+else
+    curl --proto '=https' -sSfL --tlsv1.2 "https://download.dfinity.systems/ic/${sha}/binaries/${arch}-${os}/pocket-ic.gz" -o "${outdir}/pocket-ic.gz"
+fi
+gunzip -f "${outdir}/pocket-ic.gz"
+chmod a+x "${outdir}/pocket-ic"
+
+if [[ -z "$1" ]]; then
+    tar -C dist -czf "dist/${name}.tar.gz" "${name}"
+fi
---
+++ src/main.rs
@@ -0,0 +1,262 @@
+use std::{
+    fs,
+    io::ErrorKind,
+    net::{IpAddr, SocketAddr},
+    path::PathBuf,
+    time::Duration,
+};
+
+use clap::{ArgAction, Parser, ValueEnum};
+use ic_principal::Principal;
+use notify::{Event, RecursiveMode, Watcher, recommended_watcher};
+use pocket_ic::{
+    PocketIcBuilder,
+    common::rest::{AutoProgressConfig, IcpFeatures, IcpFeaturesConfig, InstanceHttpGatewayConfig},
+};
+use reqwest::Client;
+use serde::Serialize;
+use sysinfo::{ProcessesToUpdate, Signal, System};
+use tempfile::TempDir;
+use tokio::select;
+use tokio::{process::Command, signal::unix::SignalKind};
+
+#[derive(Parser)]
+#[command(version)]
+struct Cli {
+    #[arg(long)]
+    gateway_port: Option<u16>,
+    #[arg(long)]
+    config_port: Option<u16>,
+    #[arg(long)]
+    bind: Option<IpAddr>,
+    #[arg(long)]
+    state_dir: Option<PathBuf>,
+    #[arg(long)]
+    artificial_delay_ms: Option<u64>,
+    #[arg(long, value_enum, action = ArgAction::Append)]
+    subnet: Vec<SubnetKind>,
+    #[arg(long)]
+    bitcoind_addr: Option<SocketAddr>,
+    // #[arg(long)]
+    // dogecoind_addr: Option<String>,
+    #[arg(long)]
+    ii: bool,
+    #[arg(long)]
+    nns: bool,
+    #[arg(long)]
+    pocketic_server_path: Option<PathBuf>,
+    #[arg(long)]
+    stdout_file: Option<PathBuf>,
+    #[arg(long)]
+    stderr_file: Option<PathBuf>,
+    #[arg(long)]
+    status_dir: Option<PathBuf>,
+}
+
+#[derive(ValueEnum, Clone)]
+enum SubnetKind {
+    Application,
+    System,
+    VerifiedApplication,
+    Bitcoin,
+    Fiduciary,
+    Nns,
+    Sns,
+}
+
+#[tokio::main]
+async fn main() {
+    let Cli {
+        gateway_port,
+        config_port,
+        bind,
+        state_dir,
+        artificial_delay_ms,
+        subnet,
+        bitcoind_addr,
+        // dogecoind_addr,
+        ii,
+        nns,
+        pocketic_server_path,
+        stdout_file,
+        stderr_file,
+        status_dir,
+    } = Cli::parse();
+    let pocketic_server_path = if let Some(path) = pocketic_server_path {
+        path
+    } else {
+        let assumed = std::env::current_exe()
+            .unwrap()
+            .parent()
+            .unwrap()
+            .join("pocket-ic");
+        if !assumed.exists() {
+            eprintln!(
+                "Error: --pocketic-server-path not provided and could not find pocket-ic next to the launcher"
+            );
+            std::process::exit(1);
+        }
+        assumed
+    };
+    let tmpdir = TempDir::new().unwrap();
+    let port_file = tmpdir.path().join("pocketic.port");
+    let (tx, mut rx) = tokio::sync::mpsc::channel(10);
+    let mut watcher = recommended_watcher({
+        let port_file = port_file.clone();
+        move |event: Result<Event, notify::Error>| {
+            event.unwrap();
+            match fs::read_to_string(&port_file) {
+                Ok(contents) => {
+                    if contents.ends_with('\n') {
+                        let port: u16 = contents.trim().parse().unwrap();
+                        let _ = tx.blocking_send(port);
+                    }
+                }
+                Err(e) if e.kind() == ErrorKind::NotFound => {}
+                Err(e) => panic!("Failed to read port file: {}", e),
+            };
+        }
+    })
+    .unwrap();
+    watcher
+        .watch(tmpdir.path(), RecursiveMode::Recursive)
+        .unwrap();
+    let mut cmd = Command::new(&pocketic_server_path);
+    cmd.args(["--ttl", "2592000"]);
+    if let Some(config_port) = config_port {
+        cmd.args(["--port", &config_port.to_string()]);
+    }
+    cmd.arg("--port-file").arg(&port_file);
+    if let Some(bind) = bind {
+        cmd.arg("--ip-addr").arg(bind.to_string());
+    }
+    if let Some(stdout_file) = stdout_file {
+        let file = std::fs::File::create(stdout_file).unwrap();
+        cmd.stdout(file);
+    }
+    if let Some(stderr_file) = stderr_file {
+        let file = std::fs::File::create(stderr_file).unwrap();
+        cmd.stderr(file);
+    }
+    #[cfg(unix)]
+    {
+        cmd.process_group(0);
+    }
+    let mut child = cmd.spawn().unwrap();
+    let config_port = rx.recv().await.unwrap();
+    drop(watcher);
+    let mut pic = PocketIcBuilder::new()
+        .with_server_url(format!("http://127.0.0.1:{config_port}/").parse().unwrap())
+        .with_http_gateway(InstanceHttpGatewayConfig {
+            ip_addr: bind.map(|ip| ip.to_string()),
+            port: gateway_port,
+            domains: Some(vec!["localhost".to_string()]),
+            https_config: None,
+        });
+    if let Some(dir) = state_dir {
+        pic = pic.with_state_dir(dir);
+    }
+    if subnet.is_empty() {
+        pic = pic.with_application_subnet();
+    } else {
+        for subnet in subnet {
+            match subnet {
+                SubnetKind::Application => pic = pic.with_application_subnet(),
+                SubnetKind::System => pic = pic.with_system_subnet(),
+                SubnetKind::VerifiedApplication => pic = pic.with_verified_application_subnet(),
+                SubnetKind::Bitcoin => pic = pic.with_bitcoin_subnet(),
+                SubnetKind::Fiduciary => pic = pic.with_fiduciary_subnet(),
+                SubnetKind::Nns => pic = pic.with_nns_subnet(),
+                SubnetKind::Sns => pic = pic.with_sns_subnet(),
+            }
+        }
+    }
+    pic = pic.with_nns_subnet();
+    let mut features = IcpFeatures {
+        cycles_minting: Some(IcpFeaturesConfig::DefaultConfig),
+        icp_token: Some(IcpFeaturesConfig::DefaultConfig),
+        cycles_token: Some(IcpFeaturesConfig::DefaultConfig),
+        ..<_>::default()
+    };
+    if nns || ii {
+        pic = pic.with_ii_subnet();
+        features.ii = Some(IcpFeaturesConfig::DefaultConfig);
+    }
+    if nns {
+        pic = pic.with_sns_subnet();
+        features.nns_governance = Some(IcpFeaturesConfig::DefaultConfig);
+        features.nns_ui = Some(IcpFeaturesConfig::DefaultConfig);
+        features.sns = Some(IcpFeaturesConfig::DefaultConfig);
+    }
+    pic = pic.with_icp_features(features);
+    if let Some(bitcoind_addr) = bitcoind_addr {
+        pic = pic.with_bitcoind_addr(bitcoind_addr);
+    }
+    // if let Some(dogecoind_addr) = dogecoind_addr {
+    //     pic = pic.with_dogecoind_addr(dogecoind_addr);
+    // }
+    let pic = pic.build_async().await;
+    let client = Client::new();
+    let progress_url = pic
+        .get_server_url()
+        .join(&format!("/instances/{}/auto_progress", pic.instance_id))
+        .unwrap();
+    client
+        .post(progress_url)
+        .json(&AutoProgressConfig {
+            artificial_delay_ms,
+        })
+        .send()
+        .await
+        .unwrap()
+        .error_for_status()
+        .unwrap();
+    let topology = pic.topology().await;
+    let default_ecid = Principal::from_slice(&topology.default_effective_canister_id.canister_id);
+    let gateway_url = pic.url().unwrap();
+
+    if let Some(status_dir) = status_dir {
+        let status_file = status_dir.join("status.json");
+        let status = Status {
+            v: "1".to_string(),
+            instance_id: pic.instance_id,
+            config_port,
+            gateway_port: gateway_url.port().unwrap(),
+            root_key: hex::encode(pic.root_key().await.unwrap()),
+            default_effective_canister_id: default_ecid,
+        };
+        let mut contents = serde_json::to_string(&status).unwrap();
+        contents.push('\n');
+        println!("launcher: writing status to {}", status_file.display());
+        fs::write(status_file, contents).unwrap();
+    }
+    let ctrlc = tokio::signal::ctrl_c();
+    let mut sigterm = tokio::signal::unix::signal(SignalKind::terminate()).unwrap();
+    select! {
+        _ = ctrlc => {},
+        _ = sigterm.recv() => {},
+    }
+    pic.drop().await;
+    let pid = child.id().unwrap() as usize;
+    let mut sys = System::new();
+    sys.refresh_processes(ProcessesToUpdate::Some(&[pid.into()]), true);
+    if let Some(process) = sys.process(pid.into()) {
+        process.kill_with(Signal::Interrupt);
+    }
+    select! {
+        _ = child.wait() => {},
+        _ = tokio::time::sleep(Duration::from_secs(5)) => {
+            let _ = child.kill().await;
+        }
+    }
+}
+
+#[derive(Serialize)]
+struct Status {
+    v: String,
+    instance_id: usize,
+    config_port: u16,
+    gateway_port: u16,
+    root_key: String,
+    default_effective_canister_id: Principal,
+}
