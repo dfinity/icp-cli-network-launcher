9eaea76c54280b4b3c2e852c92ed7361317b5c0c
Adam Spofford <adam.spofford@dfinity.org>
2025-12-03 06:15:02 -0800

    fg docker run, bg jq

--- run-example-docker.sh
+++ run-example-docker.sh
@@ -2,20 +2,17 @@
 set -e
 [[ $# -eq 1 ]] || { echo "Usage: '$0 <status-dir>'" >&2; exit 1; }
 d=$(mktemp -d)
+container=$(head -c 16 /dev/urandom | xxd -p)
 docker build -t pocket-ic-launcher-example -f ./example.Dockerfile .
-container=$(docker run -d -v "$d:/app/status" -P pocket-ic-launcher-example)
-trap "docker stop $container >/dev/null" EXIT
-
-until [[ -e "$d/status.json" || "$(docker container inspect -f '{{.State.Running}}' $container)" != "true" ]]; do
-    sleep 0.5
-done
-if [[ ! -e "$d/status.json" ]]; then
-    echo "Error: container exited before creating status.json" >&2
-    docker logs $container >&2
-    exit 1
-fi
-config_port=$(docker port "$container" 4942/tcp | cut -d: -f2)
-gateway_port=$(docker port "$container" 4943/tcp | cut -d: -f2)
-jq '.config_port = $config | .gateway_port = $gateway' --arg config "$config_port" --arg gateway "$gateway_port" "$d/status.json" > "$1/status.json"
-trap - EXIT
-docker attach $container
+rewrite_json() {
+    until [[ -e "$d/status.json" ]]; do
+        sleep 0.2
+    done
+    config_port=$(docker port "$container" 4942/tcp | cut -d: -f2)
+    gateway_port=$(docker port "$container" 4943/tcp | cut -d: -f2)
+    jq '.config_port = $config | .gateway_port = $gateway' \
+        --arg config "$config_port" --arg gateway "$gateway_port" "$d/status.json" > "$1/status.json"
+}
+rewrite_json&
+trap 'kill %rewrite_json' EXIT
+docker run -it -v "$d:/app/status" -P --name "$container" pocket-ic-launcher-example
